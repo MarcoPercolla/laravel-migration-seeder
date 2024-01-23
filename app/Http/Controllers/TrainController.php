6ed520436ef59937f48f30c34ae5d77c66d037f2
ReDMallard <marco@titori.it>
2024-01-24 00:55:34 +0100

    milestone 2

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class TrainController extends Controller
+{
+    public function index()
+    {
+
+        $dati = [
+            "trains" => Train::all()
+        ];
+
+        return view('home', $dati);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Dati da config/data.js: {{ $home }}</h1>
-    <p>This is my paragraph content.</p>
+<div class="container">
+    @foreach ($trains as $train)
+    <div class="card">
+        <h3>{{$train->company}}</h3>
+        <h5>Da:{{$train->departure_station}}</h5>
+        <p>partenza:{{$train->departure_time}}</p>
+        <p>diretto a:{{$train->arrival_station}}</p>
+        <p>arrivo:{{$train->arrival_time}}</p>
+        <p>code:{{$train->train_code}}</p>
+        <p>vagoni:{{$train->carriages_tot}}</p>
+        <h6>{{ $train->on_time ? 'TRENO IN ORARIO' : '' }}</h6>
+        <h6>{{ $train->delayed ? 'TRENO IN RITARDO' : '' }}</h6>
+        
+
+        
+
+
+    </div>   
+    @endforeach
+</div>
+    
 @endsection
