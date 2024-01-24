06f17df4c464ea6b772eef820358a449781f03de
ReDMallard <marco@titori.it>
2024-01-24 01:31:49 +0100

    aggiunge elemento scritto a mano con seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -14,6 +14,33 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        //
+        $trains = [
+            [
+                "company" => "Frecciarossa",
+                "departure_station" => "Milano",
+                "arrival_station" => "Bologna",
+                "departure_time" => "2024-02-02 10:23:30",
+                "arrival_time" => "2024-02-02 01:03:30",
+                "train_code" => "8343",
+                'carriages_tot' => "14",
+                "on_time" => "1",
+                "delayed" => "0",
+            ]
+
+        ];
+
+        foreach ($trains as $train) {
+            $nuovoTreno = new Train();
+            $nuovoTreno->company = $train["company"];
+            $nuovoTreno->departure_station = $train["departure_station"];
+            $nuovoTreno->arrival_station = $train["arrival_station"];
+            $nuovoTreno->departure_time = $train["departure_time"];
+            $nuovoTreno->arrival_time = $train["arrival_time"];
+            $nuovoTreno->train_code = $train["train_code"];
+            $nuovoTreno->train_code = $train["train_code"];
+            $nuovoTreno->carriages_tot = $train["carriages_tot"];
+            $nuovoTreno->delayed = $train["delayed"];
+            $nuovoTreno->save();
+        };
     }
 }
